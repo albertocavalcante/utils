721b6ea9905717fc428b09c28e59e172b21c7a3e
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2022-12-18 03:41:14 -0300

    add install_gradle.sh

---
+++ install-gradle.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+[ "$EUID" -eq 0 ] || exec sudo "$0" "$@"
+export GRADLE_VERSION=7.6
+export GRADLE_DISTRIBUTION=all
+export GRADLE_ARTIFACT="gradle-${GRADLE_VERSION}"
+export GRADLE_ZIP="${GRADLE_ARTIFACT}-${GRADLE_DISTRIBUTION}.zip"
+export GRADLE_DISTRIBUTION_URL="https://services.gradle.org/distributions/${GRADLE_ZIP}"
+
+export GRADLE_HOME_DIR=/opt/gradle
+
+export TMP_DIR="$(mktemp -d -t gradle.XXXXXX)"
+export GRADLE_ZIP_DOWNLOAD_LOCATION="${TMP_DIR}/${GRADLE_ZIP}"
+
+[ -d "$GRADLE_HOME_DIR" ] || mkdir -p "$GRADLE_HOME_DIR"
+
+wget "$GRADLE_DISTRIBUTION_URL" -O "$GRADLE_ZIP_DOWNLOAD_LOCATION"
+
+unzip -d "$GRADLE_HOME_DIR" "$GRADLE_ZIP_DOWNLOAD_LOCATION"
+
+ls -l "${GRADLE_HOME_DIR}/${GRADLE_ARTIFACT}"
+
+# FISH
+# fish_add_path /opt/gradle/gradle-7.6/bin
