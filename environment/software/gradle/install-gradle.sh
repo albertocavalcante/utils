7c1853195c509b416cce6674c7c484c586298b94
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2023-11-18 20:51:47 -0300

    upgrade gradle to 8.4

--- environment/software/gradle/install-gradle.sh
+++ environment/software/gradle/install-gradle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ "$EUID" -eq 0 ] || exec sudo "$0" "$@"
-export GRADLE_VERSION=7.6
+export GRADLE_VERSION=8.4
 export GRADLE_DISTRIBUTION=all
 export GRADLE_ARTIFACT="gradle-${GRADLE_VERSION}"
 export GRADLE_ZIP="${GRADLE_ARTIFACT}-${GRADLE_DISTRIBUTION}.zip"
@@ -21,4 +21,4 @@ unzip -d "$GRADLE_HOME_DIR" "$GRADLE_ZIP_DOWNLOAD_LOCATION"
 ls -l "${GRADLE_HOME_DIR}/${GRADLE_ARTIFACT}"
 
 # FISH
-# fish_add_path /opt/gradle/gradle-7.6/bin
+# fish_add_path /opt/gradle/gradle-8.4/bin
