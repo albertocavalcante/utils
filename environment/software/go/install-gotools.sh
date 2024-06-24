b0f4b6506d8286838c32846f4d7b6ed99ca1da1c
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2024-06-24 03:04:06 -0300

    add more tools to install-gotools.sh

--- environment/software/go/install-gotools.sh
+++ environment/software/go/install-gotools.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env -S bash -ex
 
 urls=(
+    "golang.org/x/tools/gopls@latest"
+    "golang.org/dl/gotip@latest"
+
+    "github.com/go-delve/delve/cmd/dlv@latest"
+
+    "honnef.co/go/tools/cmd/staticcheck@latest"
+
     "github.com/spf13/cobra-cli@latest"
+
     "github.com/bazelbuild/buildtools/buildozer@latest"    
     "github.com/bazelbuild/buildtools/buildifier@latest"
 )
