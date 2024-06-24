cc30e871641785cf3850e882d6586717f0800073
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2024-06-24 03:29:23 -0300

    install-go.sh: handle when dir already exists // prompt if should overwrite it

--- environment/software/go/install-go.sh
+++ environment/software/go/install-go.sh
@@ -26,6 +26,23 @@ tar -xvf $GOTARGZ
 
 SHORTVERSION=$(echo $GOVERSION | cut -d'.' -f1,2)
 mv go go-$SHORTVERSION
-sudo mv go-$SHORTVERSION /usr/local
+
+TARGET_DIR="/usr/local/go-$SHORTVERSION"
+
+if [ -d $TARGET_DIR ]; then
+  read -p "$TARGET_DIR already exists. Overwrite? [y/n] " -n 1 -r answer
+
+  if [[ $answer =~ ^[Yy]$ ]]; then
+    rm -rf $TARGET_DIR
+    mv go-$SHORTVERSION /usr/local
+    echo "Replaced $TARGET_DIR with the new Go version ($GOVERSION)."
+  else
+    echo -e "\nAborted."
+    exit 1
+  fi
+else
+  mv go-$SHORTVERSION /usr/local
+  echo "Installed Go $GOVERSION to $TARGET_DIR."
+fi
 
 popd
